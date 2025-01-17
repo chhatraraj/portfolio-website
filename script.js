5197299321224b2fc6fdd6daff97e8749236be98
chhatraraj <chhatraneupane999@gmail.com>
2025-01-17 22:27:20 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// document.getElementById('contact-form').addEventListener('submit', function (e) {
+//     e.preventDefault(); // Prevent default form submission
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+  
+//     if (name && email) {
+//       alert(`Thank you, ${name}! Your message has been sent successfully.`);
+//     } else {
+//       alert('Please fill in all the fields before submitting.');
+//     }
+//   });
+
+  
+
+//   // Smooth Scroll for Navbar Links
+// const navLinks = document.querySelectorAll('.nav-links a');
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const targetId = this.getAttribute('href');
+//     const targetSection = document.querySelector(targetId);
+//     window.scrollTo({
+//       top: targetSection.offsetTop - 60, // Adjust for fixed navbar
+//       behavior: 'smooth'
+//     });
+//   });
+// });
